a2c6683508819e865c4d5821eb7012851fad5000
Dalibi <130359972+dalibi120707@users.noreply.github.com>
2023-04-18 16:37:22 +0600

    Add files via upload

---
+++ DZ-4/app.js
@@ -0,0 +1,22 @@
+var arr = [null,  undefined, null, "qwerty", 34, 65, undefined, null, null, 43, "aerg", "пкцйк", null, 'uioo']
+const obj=[], numb=[], str=[], undef=[], newArray=[]
+for (let i of arr){
+  if(typeof i === 'object') {
+    obj.push(i)
+  }else if(typeof i=== 'number') {
+    numb.push(i)
+  }else if(typeof i === 'string'){
+    str.push(i)
+  }else if(typeof i === 'undefined'){
+    undef.push(i)
+  }
+}
+newArray.push(obj,numb,str,undef)
+
+const res = newArray.sort((a, b) => b.length - a.length)
+console.log(res);
+
+
+
+
+
